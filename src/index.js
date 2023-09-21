846695c936ff5f59595838dd4f62695aa8e57141
Yura <114563784+YuraKravchuk@users.noreply.github.com>
2023-09-21 11:15:26 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -87,11 +87,10 @@ async function resultQuery() {
 
     if (currentPage >= totalHits / 40) {
       refs.loadMore.classList.add('hidden');
+      refs.loadMore.removeEventListener('click', handleClick);
       Notiflix.Notify.failure(
         "We're sorry, but you've reached the end of search results."
       );
-    } else {
-      refs.loadMore.classList.remove('hidden');
     }
   } catch (error) {
     Notiflix.Notify.failure(
